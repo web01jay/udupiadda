ce289bb5c90798e22297460bc39d98852ff95e64
Jay Panchal <jaypanchal@Jays-Mac-mini.local>
2024-08-27 04:01:21 +0530

    other pages added

---
+++ js/menu.js
@@ -0,0 +1,274 @@
+const menuData = [
+    {
+        "title": "APPETIZERS",
+        "dish": [
+            { "name": "Idli", "description": "Idli", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Sambhar Idli", "description": "Sambhar Idli", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Rasam Idli", "description": "Rasam Idli", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Podi Idli", "description": "Podi Idli", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Dahi Idli", "description": "Dahi Idli", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Medhu Vada", "description": "Medhu Vada", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Sambhar Vada", "description": "Sambhar Vada", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Rasam Vada", "description": "Rasam Vada", "imageLink": "./images/dishes/stockcopy.jpeg" },
+        ]
+    },
+    {
+        "title": "Combo",
+        "dish": [
+            { "name": "1 Idli & 1 Medhu Vada", "description": "1 Idli & 1 Medhu Vada", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Choose any 3", "description": "Choose any 3", "imageLink": "./images/dishes/stockcopy.jpeg" },
+        ]
+    },
+    {
+        "title": "Soup",
+        "dish": [
+            { "name": "Rasam Soup", "description": "Rasam Soup", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Lentil Soup", "description": "Lentil Soup", "imageLink": "./images/dishes/stockcopy.jpeg" },
+        ]
+    },
+    {
+        "title": "Mumbai Chaat",
+        "dish": [
+            { "name": "Vada Pav", "description": "Vada Pav", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Samosa Chaat", "description": "Samosa Chaat", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Papdi Chaat", "description": "Papdi Chaat", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Pani Puri", "description": "Pani Puri", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Chinese Bhel Puri", "description": "Chinese Bhel Puri", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Dahi Vada", "description": "Dahi Vada", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Upma", "description": "Upma", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Pav Bhaji", "description": "Pav Bhaji", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Idli Manchurian", "description": "Idli Manchurian", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Veg Manchurian", "description": "Veg Manchurian", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Paneer Chilli Manchurian", "description": "Paneer Chilli Manchurian", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Veg Samosa", "description": "Veg Samosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Paneer Tikka Samosa", "description": "Paneer Tikka Samosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Onion Pakora", "description": "Onion Pakora", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Poori Bhaji / Korma", "description": "Poori Bhaji / Korma", "imageLink": "./images/dishes/stockcopy.jpeg" },
+        ]
+    },
+    {
+        "title": "North Indian Snacks",
+        "dish": [
+            { "name": "Chana Bhatura", "description": "Chana Bhatura", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Channa Puri", "description": "Channa Puri", "imageLink": "./images/dishes/stockcopy.jpeg" },
+        ]
+    },
+    {
+        "title": "Dosa",
+        "dish": [
+            { "name": "Sada Dosa", "description": "Sada Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Masala Dosa", "description": "Masala Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Onion Masala Dosa", "description": "Onion Masala Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Ghee Roast Dosa", "description": "Ghee Roast Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Ghee Roast Masala Dosa", "description": "Ghee Roast Masala Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Gun Powder Dosa", "description": "Gun Powder Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Gun Powder Masala Dosa", "description": "Gun Powder Masala Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Paper Dosa", "description": "Paper Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Paper Masala Dosa", "description": "Paper Masala Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Sada Mysore Dosa", "description": "Sada Mysore Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Mysore Onion Dosa", "description": "Mysore Onion Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Mysore Masala Dosa", "description": "Mysore Masala Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Mysore Onion Masala Dosa", "description": "Mysore Onion Masala Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Rava Dosa", "description": "Rava Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Rava Onion Dosa", "description": "Rava Onion Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Rava Masala Dosa", "description": "Rava Masala Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Rava Onion Masala Dosa", "description": "Rava Onion Masala Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Paneer Rava Dosa", "description": "Paneer Rava Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+        ]
+    },
+    {
+        "title": "Uttapam",
+        "dish": [
+            { "name": "Plain Uttapam", "description": "Plain Uttapam", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Onion Uttapam", "description": "Onion Uttapam", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            {
+                "name": "Onion Chilli Uttapam", "description": "Onion Chilli Uttapam", "imageLink": "./ images / dishes / Onion Chilli Uttapam.png"
+            },
+            { "name": "Tomato Uttapam", "description": "Tomato Uttapam", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Cheese Chili Uttapam", "description": "Cheese Chili Uttapam", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Gun Powder Uttapam", "description": "Gun Powder Uttapam", "imageLink": "./images/dishes/stockcopy.jpeg" },
+        ]
+    },
+    {
+        "title": "Chef's Special",
+        "dish": [
+            { "name": "Cheese Dosa", "description": "Cheese Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Cheese Chilli Dosa", "description": "Cheese Chilli Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Green Chilli Coriander Dosa", "description": "Green Chilli Coriander Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Garlic Roast Dosa Ghee / Butter", "description": "Garlic Roast Dosa Ghee / Butter", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Garlic Roast Masala Dosa", "description": "Garlic Roast Masala Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Manchurian Dosa", "description": "Manchurian Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Schezwan Noodle Dosa", "description": "Schezwan Noodle Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Pav Bhaji Dosa", "description": "Pav Bhaji Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Schezwan Dosa", "description": "Schezwan Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Paneer Dosa", "description": "Paneer Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Chef's Special Uttapam", "description": "Chef's Special Uttapam", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Nilgiri Masala Dosa", "description": "Nilgiri Masala Dosa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Tomato Omelette", "description": "Tomato Omelette", "imageLink": "./images/dishes/stockcopy.jpeg" },
+        ]
+    },
+    {
+        "title": "Rice Specialities",
+        "dish": [
+            { "name": "Steamed Rice", "description": "Steamed Rice", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Lemon Rice", "description": "Lemon Rice", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Tamarind Rice", "description": "Tamarind Rice", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Tava Pulao", "description": "Tava Pulao", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Veg Biryani", "description": "Veg Biryani", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Sambhar Rice", "description": "Sambhar Rice", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Curd Rice", "description": "Curd Rice", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Coconut Rice", "description": "Coconut Rice", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Fried Rice", "description": "Fried Rice", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Triple Schezwan", "description": "Triple Schezwan", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Schezwan Noodles", "description": "Schezwan Noodles", "imageLink": "./images/dishes/stockcopy.jpeg" },
+        ]
+    },
+    {
+        "title": "Curry Dishes",
+        "dish": [
+            { "name": "Aloo Matar", "description": "Aloo Matar", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Aloo Gobi", "description": "Aloo Gobi", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Channa Masala", "description": "Channa Masala", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Dal Tadka", "description": "Dal Tadka", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Dal Tadka with Pure Ghee", "description": "Dal Tadka with Pure Ghee", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Saag Paneer", "description": "Saag Paneer", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Aloo Saag", "description": "Aloo Saag", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Mutter Paneer", "description": "Mutter Paneer", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Paneer Tikka Kadai Masala Paneer", "description": "Paneer Tikka Kadai Masala Paneer", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Paneer Makhni", "description": "Paneer Makhni", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Veg Makhni", "description": "Veg Makhni", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Veg Korma", "description": "Veg Korma", "imageLink": "./images/dishes/stockcopy.jpeg" },
+        ]
+    },
+    {
+        "title": "Drinks",
+        "dish": [
+            { "name": "Tea (Regular / Masala)", "description": "Tea (Regular / Masala)", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Indian Coffee", "description": "Indian Coffee", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Lassi (Sweet / Salty)", "description": "Lassi (Sweet / Salty)", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Kulukki Sharbat", "description": "Kulukki Sharbat", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Mango Lassi", "description": "Mango Lassi", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Mango Shake", "description": "Mango Shake", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Virgin Pinacolada", "description": "Virgin Pinacolada", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Coconut Water", "description": "Coconut Water", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Ginger Fresh Lime Soda (Sweet / Salty)", "description": "Ginger Fresh Lime Soda (Sweet / Salty)", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Pops (Sprite, Coke, Diet Coke, Ginger Ale)", "description": "Pops (Sprite, Coke, Diet Coke, Ginger Ale)", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Bottle of Water", "description": "Bottle of Water", "imageLink": "./images/dishes/stockcopy.jpeg" },
+        ]
+    },
+    {
+        "title": "Indian Breads",
+        "dish": [
+            {
+                "name": "Aloo Paratha", "description": "Aloo Paratha", "imageLink": "./images/dishes/stockcopy.jpeg"
+            },
+            { "name": "Tava Roti", "description": "Tava Roti", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Poori", "description": "Poori", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Bhatura", "description": "Bhatura", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Naan", "description": "Naan", "imageLink": "./images/dishes/stockcopy.jpeg" },
+        ]
+    },
+    {
+        "title": "Dessert",
+        "dish": [
+            { "name": "Gulab Jamun", "description": "Gulab Jamun", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Gajar Halwa", "description": "Gajar Halwa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Rasmalai", "description": "Rasmalai", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Vanilla Ice Cream", "description": "Vanilla Ice Cream", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Kesari Sheera / Halwa", "description": "Kesari Sheera / Halwa", "imageLink": "./images/dishes/stockcopy.jpeg" },
+            { "name": "Gadbad", "description": "Gadbad", "imageLink": "./images/dishes/stockcopy.jpeg" },
+        ]
+    }
+]
+
+// const animations = ['fade-up', 'zoom-in', 'flip-left', 'slide-right'];
+const animations = ['zoom-in'];
+
+function createMenu() {
+    const menuContainer = document.getElementById('menuContainer');
+
+    menuData.forEach(category => {
+        // Create and append the title
+        const title = document.createElement('h1');
+        title.textContent = category.title;
+        title.className = 'text-center '
+        menuContainer.appendChild(title);
+
+        // Create a row for cards
+        const row = document.createElement('div');
+        row.className = 'row';
+
+        category.dish.forEach(dish => {
+            const randomAnimation = animations[Math.floor(Math.random() * animations.length)];
+            // Create a column for each card
+            const col = document.createElement('div');
+            col.className = 'col-md-4 mb-4';
+
+            // Create the card element
+            const card = document.createElement('div');
+            card.className = 'card h-100 menu-card bg-white border-0 overflow-hidden ';
+            card.setAttribute('data-aos', randomAnimation);
+            card.setAttribute('style', 'border-radius: 10px')
+
+            const imgBlock = document.createElement('div');
+            imgBlock.className = "img-block";
+
+            // Add image to the card
+            const img = document.createElement('img');
+            img.src = dish.imageLink;
+            img.className = 'card-img-top';
+            img.alt = dish.name;
+            imgBlock.append(img)
+            card.appendChild(imgBlock);
+
+            // Create card body
+            const cardBody = document.createElement('div');
+            cardBody.className = 'card-body';
+
+            // Add name to the card
+            const cardTitle = document.createElement('h5');
+            cardTitle.className = 'card-title text-center font-bold';
+            cardTitle.textContent = dish.name;
+            cardBody.appendChild(cardTitle);
+
+            // Add description to the card
+            const cardText = document.createElement('p');
+            cardText.className = 'card-text text-center text-grey';
+            cardText.textContent = dish.description;
+            cardBody.appendChild(cardText);
+
+            // Add rate nd btn container
+            const rateContainer = document.createElement('div');
+            rateContainer.className = 'd-flex align-items-center justify-content-between'
+
+            // Add Dish Rate
+            const dishRate = document.createElement('span');
+            dishRate.className = 'text-black'
+            dishRate.textContent = '$20'
+            rateContainer.appendChild(dishRate);
+
+            // Add to cart button
+            const cartButton = document.createElement('button');
+            cartButton.className = 'border-0 btn text-center px-3 ml-auto'
+            cartButton.setAttribute('style', "background-color: #4C593E; color: #fff; border-radius: 5px");
+            cartButton.textContent = "+ Add"
+            rateContainer.appendChild(cartButton);
+            cardBody.appendChild(rateContainer);
+
+            // Append card body to card
+            card.appendChild(cardBody);
+
+            // Append card to column
+            col.appendChild(card);
+
+            // Append column to row
+            row.appendChild(col);
+        });
+
+        // Append row to menu container
+        menuContainer.appendChild(row);
+    });
+}
+
+// Call the function to create the menu
+createMenu();
